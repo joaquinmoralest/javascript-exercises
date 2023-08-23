ef04c61abc6877a133bafe3329d2069320d647ea
Joaquin Morales <joaquin.moralestr@gmail.com>
2023-08-23 09:37:10 -0400

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function (...numbers) {
+  if (numbers.some((number) => number < 0 || typeof number != 'number')) {
+    return 'ERROR'
+  } else {
+    const sortedNumber = numbers.sort()
+    let total = 0
+    for (let i = sortedNumber[0]; i <= sortedNumber[1]; i++) {
+      total += i
+    }
+    return total
+  }
 };
 
 // Do not edit below this line
