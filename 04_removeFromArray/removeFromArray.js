9d0913da3b80f55aed4648e271273fcffb73c4e1
Joaquin Morales <joaquin.moralestr@gmail.com>
2023-08-23 02:06:45 -0400

    Finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...elements) {
+  let newArray = []
+  array.forEach(element => {
+    if (!elements.includes(element)) {
+      newArray.push(element)
+    }
+  })
+  return newArray
 };
 
 // Do not edit below this line
