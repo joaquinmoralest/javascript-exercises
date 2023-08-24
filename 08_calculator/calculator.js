4804f7ff3cb06f07848bf0379e8ac5bf5b75aa74
Joaquin Morales <joaquin.moralestr@gmail.com>
2023-08-23 22:44:28 -0400

    Finish exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let sum = 0
+  if (array.length > 0) {
+    array.forEach((el) => {
+      sum += el
+    })
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let total = 1
+  if (array.length > 0) {
+    array.forEach((el) => {
+      total *= el
+    })
+  }
+  return total
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function (max) {
+  let numbers = []
+  let total = 1
+  for (let i = 1; i <= max; i++) {
+    numbers.push(i)
+  }
+  numbers.forEach((el) => {
+    total *= el
+  })
+  return total
 };
 
 // Do not edit below this line
