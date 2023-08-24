2da2c86006c5c49254a6b40d0d09029a64780c54
Joaquin Morales <joaquin.moralestr@gmail.com>
2023-08-24 00:01:33 -0400

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  let a = 1
+  let b = 1
+  if (n >= 0) {
+    for (let i = 3; i <= n; i++) {
+      let c = a + b
+      a = b
+      b = c
+    }
+    return b
+  }
+  return 'OOPS'
 };
 
 // Do not edit below this line
