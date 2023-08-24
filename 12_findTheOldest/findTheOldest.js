0f9d63da8d1664ce54100748a14d64736f4c9057
Joaquin Morales <joaquin.moralestr@gmail.com>
2023-08-24 01:04:22 -0400

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+  return array.reduce((oldest, current) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath)
+    const currentAge = getAge(current.yearOfBirth, current.yearOfDeath)
+    return oldestAge > currentAge ? oldest : current
+  })
 };
+
+const getAge = (yearOfBirth, yearOfDeath) => {
+  if (!yearOfDeath) {
+    yearOfDeath = new Date().getFullYear()
+  }
+  return yearOfDeath - yearOfBirth
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
